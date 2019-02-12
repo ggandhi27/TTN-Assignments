1f8767cb285e0c80a799ef91be713eba62884d87
Gaurav Gandhi <grv.gandhi.27@gmail.com>
2019-02-12 14:18:57 +0530

    Javascript assignment completed

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+(function() {
+ 'use strict';
+
+ // Do not write anything above this line and do not delete it
+
+
+ // This is our note pad for testing JS
+ // Write your code here
+ 
+ window.alert('Welcome interns');
+ console.log('We will use console api for logging purposes');
+
+ 
+ // Do not write anything below this line and do not delete it
+
+}());
+
