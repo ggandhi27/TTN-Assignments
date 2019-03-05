ecde84c4a265053fc9f94c7bee9e1a96dc7c9156
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-05 13:53:14 +0530

    Commit done

--- Gradle Practice/bootcamp/source/main/java/com/intelligrape/gradle/Application.java
+++ Gradle Practice/bootcamp/source/main/java/com/intelligrape/gradle/Application.java
@@ -2,6 +2,6 @@ package com.intelligrape.gradle;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello Grade");
+        System.out.println("Hello Gradle");
     }
 }
