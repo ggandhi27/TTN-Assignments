4b8420bad6eaa818c6d6d291244b75da312f3aa5
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-06 19:27:22 +0530

    Gradle assignment completed

---
+++ Gradle Practice/Gradle_Assignment/src/main/java2/Question4.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Question4 {
+    public static void main(String[] args) {
+        String str = new String("Gaurav");
+        System.out.println(StringUtils.equals(str,"Gaurav"));
+    }
+}
