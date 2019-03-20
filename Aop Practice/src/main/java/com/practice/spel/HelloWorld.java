522ea389730bc3bc1114684a498606eb44d5ec0a
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-21 02:48:04 +0530

    ftbkythv

--- Aop Practice/src/main/java/com/practice/spel/HelloWorld.java
+++ Aop Practice/src/main/java/com/practice/spel/HelloWorld.java
@@ -1,4 +1,19 @@
 package com.practice.spel;
 
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+    
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression("'Hello world'");
+        
+        String message = (String) expression.getValue();
+        System.out.println(message);
+        
+        Expression expression1 = expressionParser.parseExpression("{1,2,3,4}");
+        System.out.println(expression1.getValue());
+    }
 }
