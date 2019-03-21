d0e2ea83a125854026f584d2b33d478c2b7cf0d5
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-21 14:03:47 +0530

    Question1 done and dusted

--- Aop Assignment/src/main/java/Question1.java
+++ Aop Assignment/src/main/java/Question1.java
@@ -1,2 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Question1 {
+    public static void main(String[] args) {
+    
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("question1.xml");
+        ((ClassPathXmlApplicationContext) applicationContext).start();
+        ((ClassPathXmlApplicationContext) applicationContext).stop();
+        ((ClassPathXmlApplicationContext) applicationContext).close();
+    }
 }
