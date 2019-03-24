a030b31317659816a7f48fa744a0a3722389c7e4
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-24 18:04:05 +0530

    Question 6 for Spring MVC Assignment done and dusted

--- Spring MVC Assignment/src/main/java/com/controller/question3/StudentController.java
+++ Spring MVC Assignment/src/main/java/com/controller/question3/StudentController.java
@@ -1,9 +1,14 @@
 package com.controller.question3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class StudentController {
@@ -26,4 +31,16 @@ public class StudentController {
     @RequestMapping("/HelloWorld")
     @ResponseBody
     public ModelAndView getHelloWorld() { return new ModelAndView("helloworld");}
+    
+//    Question 6
+    @RequestMapping("/details/{firstname}/{lastname}")
+    @ResponseBody
+    public String getDetails(@PathVariable String firstname,@PathVariable String lastname) {
+        return "First Name :: " + firstname + "  <br>Last Name :: " + lastname;
+    }
+//    public void getDetails(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+//                           @PathVariable String firstname, @PathVariable String lastname) throws IOException {
+//        httpServletResponse.setContentType("text/html");
+//        httpServletResponse.getWriter().println("First Name :: " + firstname + "<br>Last Name :: " + lastname);
+//    }
 }
