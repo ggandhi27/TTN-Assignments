e2c45ec1c37aae5bfde7f5d94771f2c2a33e7ed2
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-25 00:35:53 +0530

    Question 6 done and dusted

--- Spring Boot Assignment/src/main/java/com/spring/boot/assignment/springbootassignment/controller/DatabaseController.java
+++ Spring Boot Assignment/src/main/java/com/spring/boot/assignment/springbootassignment/controller/DatabaseController.java
@@ -1,6 +1,8 @@
 package com.spring.boot.assignment.springbootassignment.controller;
 
 import com.spring.boot.assignment.springbootassignment.entity.Database;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DatabaseController {
     
+    Logger logger = LoggerFactory.getLogger(DatabaseController.class);
+    
     @Autowired
     Database database;
     
     @GetMapping("/getdb")
     public String getdb() {
+        logger.trace("getdb method is invoked.");
+        logger.debug("a debug msg for getdb");
+        logger.info("This is a get db method.");
+        logger.warn("A warning occurred in getdb.");
+        logger.error("An error occurred. in getdb");
         return  database.toString();
     }
     
