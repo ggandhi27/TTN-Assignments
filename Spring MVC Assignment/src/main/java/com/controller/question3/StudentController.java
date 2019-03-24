3d02d9872ac4e3d4445a85fea6aa336294b7fab8
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-24 17:52:04 +0530

    Question 4 Spring MVC Assignment done and dusted

--- Spring MVC Assignment/src/main/java/com/controller/question3/StudentController.java
+++ Spring MVC Assignment/src/main/java/com/controller/question3/StudentController.java
@@ -2,15 +2,24 @@ package com.controller.question3;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
-    
+
+//    Question 3
     @RequestMapping("/")
     public ModelAndView getIndex() {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
     }
+    
+//    Question 4
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String getHello() {
+        return "Hello World";
+    }
+    
 }
