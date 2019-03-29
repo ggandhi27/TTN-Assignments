0df72a6716559bbc9b74c301a00130efe47355d0
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-29 13:27:58 +0530

    Question 2 for Hibernate Assignment done and dusted

--- Hibernate Assignment/src/main/java/com/hibernate/assignment/application/Application.java
+++ Hibernate Assignment/src/main/java/com/hibernate/assignment/application/Application.java
@@ -13,13 +13,32 @@ public class Application {
         author.setAge(59);
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
-        if(isCreated(session,author)) {
+        if (isCreated(session, author)) {
             System.out.println(author + " is inserted successfully.");
         }
+        read(session, 1);
+        if(isDeleted(session,1)) {
+            System.out.println("Row with id " + 1 + " is deleted.");
+        }
     }
+    
     static boolean isCreated(Session session, Author author) {
         session.beginTransaction();
         session.save(author);
+        session.getTransaction().commit();
+        return true;
+    }
+    
+    static void read(Session session, int id) {
+        session.beginTransaction();
+        Author author = session.get(Author.class, id);
+        System.out.println(author);
+        session.getTransaction().commit();
+    }
+    
+    static boolean isDeleted(Session session, int id) {
+        session.beginTransaction();
+        session.delete(session.get(Author.class,id));
         session.getTransaction().commit();
         return true;
     }
