27632a58258f9b167691c43c7e4c857eed12d582
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-29 12:09:31 +0530

    Question 1 for Hibernate Assignment done and dusted

---
+++ Hibernate Assignment/src/test/java/com/hibernate/assignment/hibernateassignment/HibernateassignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hibernate.assignment.hibernateassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith (SpringRunner.class)
+@SpringBootTest
+public class HibernateassignmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
