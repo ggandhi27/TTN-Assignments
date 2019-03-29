2aa93d4d78ab92bd0c418f7af23a3332deedbeea
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-03-29 13:10:58 +0530

    Question 2 done and dusted

--- Hibernate Assignment/src/main/java/com/hibernate/assignment/application/Application.java
+++ Hibernate Assignment/src/main/java/com/hibernate/assignment/application/Application.java
@@ -1,4 +1,26 @@
 package com.hibernate.assignment.application;
 
+import com.hibernate.assignment.hibernateassignment.entity.Author;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Application {
+    public static void main(String[] args) {
+        Author author = new Author();
+        author.setFirstname("George");
+        author.setLastname("Martin");
+        author.setAge(59);
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        if(isCreated(session,author)) {
+            System.out.println(author + " is inserted successfully.");
+        }
+    }
+    static boolean isCreated(Session session, Author author) {
+        session.beginTransaction();
+        session.save(author);
+        session.getTransaction().commit();
+        return true;
+    }
 }
--- Hibernate Assignment/src/main/java/com/hibernate/assignment/hibernateassignment/entity/Author.java
+++ Hibernate Assignment/src/main/java/com/hibernate/assignment/hibernateassignment/entity/Author.java
@@ -1,14 +1,12 @@
 package com.hibernate.assignment.hibernateassignment.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "author")
 public class Author {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstname;
     private String lastname;
